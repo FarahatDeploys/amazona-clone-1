077102592ae0d2a8520728fd36e8935c31381be7
Hady Farahat <farhat2312@gmail.com>
2022-02-01 00:33:48 +0200

    commit on master

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import data from "./data";
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
 import { createSlice } from "@reduxjs/toolkit";
-const initialState = [];
-const dataReducer = createSlice({
-  name: "datareducers",
+import { compose, createStore } from "redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+const initialState = {};
+// const dataReducer = createSlice({
+//   name: "datareducers",
+//   initialState,
+//   reducers: {
+//     datareducer: (state, action) => {
+//       return data.products;
+//     },
+//   },
+// });
+const reducer = (state, action) => {
+  return { product: data.products };
+};
+// const store = configureStore({
+//   reducer: dataReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       thunk,
+//     }),
+// });
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
   initialState,
-  reducers: {
-    datareducer: (state, action) => {
-      return data.products;
-    },
-  },
-});
-const store = configureStore({ reducer: dataReducer });
+  composeEnhancer(applyMiddleware(thunk))
+);
 export default store;
